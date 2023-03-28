15a3031c203148cb99c3f01f676ec3eb04ca25bb
JCastle <jcastle.ahn@gmail.com>
2023-03-28 14:47:37 +0900

    change channel id and token

--- msit/msit.py
+++ msit/msit.py
@@ -12,8 +12,10 @@ chrome_options.add_argument("--headless")
 driver = webdriver.Chrome(options=chrome_options)
 
 # telegram info
-telegram_bot_token = '5895967085:AAFJY2ln43kPMf4EqYV-XM6j8tXoVMuu1yE'
-telegram_chat_id = '6173488270'
+f = open('/home/ubuntu/crawling/token.txt', mode='rt', encoding='utf-8')
+telegram_bot_token = f.read().splitlines()[0]
+#telegram_bot_token = '5895967085:AAFJY2ln43kPMf4EqYV-XM6j8tXoVMuu1yE'
+telegram_chat_id = '-1001835326415'
 
 # send telegram message
 def send(title,a_tag_number,page):
